df6b96d804c2b7ed71ebdd60702ab782db7a8c4a
ramonornelas <ornelas@pythian.com>
2023-10-09 17:56:44 -0600

    fix1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const JsonConsumer = () => {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    (async function () {
-      const { text } = await (await fetch("/api/message")).json();
-      setData(text);
-    })();
-  });
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/message");
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures that this effect runs once when the component mounts
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
 
   return (
     <div>
-      <h1>Datos JSON de API Local</h1>
+      <h1>Datos de JSON API Consumer</h1>
       <ul>
         {data.map((item) => (
-          <li>{item.name}</li>
+          <li>{item.name} {item.lastname}</li>
         ))}
       </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default JsonConsumer;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import JsonConsumer from './JsonConsumer';
 
-ReactDOM.render(<JsonConsumer />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
